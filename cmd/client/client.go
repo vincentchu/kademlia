c033a7d3f7ba3057d7bf914b257ee0ee316e2896
Vincent Chu <vincentchu@gmail.com>
2018-07-07 00:50:52 +0200

    Do some refactoring on client

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -5,45 +5,24 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"math/rand"
 	"time"
 	// "time"
 
 	dht "gx/ipfs/QmNg6M98bwS97SL9ArvrRxKujFps3eV6XvmKgduiYga8Bn/go-libp2p-kad-dht"
 	dhtopts "gx/ipfs/QmNg6M98bwS97SL9ArvrRxKujFps3eV6XvmKgduiYga8Bn/go-libp2p-kad-dht/opts"
 	// net "gx/ipfs/QmPjvxTpVH8qJyQDnxnsxF9kv9jezKD1kozz1hs3fCGsNh/go-libp2p-net"
-	multiaddr "gx/ipfs/QmYmsdtJ3HsodkePE3eU3TsCaP2YvPZJ4LoXnNkDE5Tpt7/go-multiaddr"
+
 	// protocol "gx/ipfs/QmZNkThpqfVXs9GNbexPrfBbXSLNYeKrE7jwFM2oqHbyqN/go-libp2p-protocol"
 
 	host "gx/ipfs/Qmb8T6YBBsjYsVGfrihQLfCJveczZnneSBqBKkYEBWDjge/go-libp2p-host"
-	peer "gx/ipfs/QmdVrMn1LhB4ybb8hMVaMLXnA8XRSewMnK6YqXKXoTcRvN/go-libp2p-peer"
-
-	crypto "gx/ipfs/Qme1knMqwt1hKZbc1BmQFmnm9f36nyQGwXxPGVpVJ9rMK5/go-libp2p-crypto"
 
 	libp2p "github.com/libp2p/go-libp2p"
+	"github.com/vincentchu/kademlia/utils"
 	// record "gx/ipfs/QmVsp2KdPYE6M8ryzCk5KHLo3zprcY5hBDaYx6uPCFUdxA/go-libp2p-record"
 )
 
-// NullValidator is a validator that does no valiadtion
-type NullValidator struct{}
-
-// Validate always returns success
-func (nv NullValidator) Validate(key string, value []byte) error {
-	log.Printf("NullValidator Validate: %s - %v\n", key, value)
-	return nil
-}
-
-// Select always selects the first record
-func (nv NullValidator) Select(key string, values [][]byte) (int, error) {
-	log.Printf("NullValidator Select: %s - %v\n", key, values)
-	log.Printf("NullValidator Select: %d", len(values))
-
-	return 0, nil
-}
-
 func makeHost(ctx context.Context) host.Host {
-	randBytes := rand.New(rand.NewSource(999))
-	prvKey, _, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, randBytes)
+	prvKey := utils.GeneratePrivateKey(999)
 
 	h, err := libp2p.New(ctx, libp2p.Identity(prvKey))
 	if err != nil {
@@ -51,34 +30,6 @@ func makeHost(ctx context.Context) host.Host {
 	}
 
 	return h
-}
-
-func makePeer(dest string) (peer.ID, multiaddr.Multiaddr) {
-	ipfsAddr, err := multiaddr.NewMultiaddr(dest)
-	if err != nil {
-		log.Fatalf("Err on creating host: %v\n", err)
-	}
-	log.Printf("Parsed: ipfsAddr = %s\n", ipfsAddr)
-
-	peerIDStr, err := ipfsAddr.ValueForProtocol(multiaddr.P_IPFS)
-	if err != nil {
-		log.Fatalf("Err on creating peerIDStr: %v\n", err)
-	}
-	log.Printf("Parsed: PeerIDStr = %s\n", peerIDStr)
-
-	peerID, err := peer.IDB58Decode(peerIDStr)
-	if err != nil {
-		log.Fatalf("Err on decoding %s: %v\n", peerIDStr, err)
-	}
-	log.Printf("Created peerID = %s\n", peerID)
-
-	targetPeerAddr, err := multiaddr.NewMultiaddr(fmt.Sprintf("/ipfs/%s", peer.IDB58Encode(peerID)))
-	log.Printf("Created targetPeerAddr = %v\n", targetPeerAddr)
-
-	targetAddr := ipfsAddr.Decapsulate(targetPeerAddr)
-	log.Printf("Decapsuated = %v\n", targetAddr)
-
-	return peerID, targetAddr
 }
 
 func parseCmd(tokens []string) (string, string, string) {
@@ -101,13 +52,10 @@ func main() {
 	defer cancel()
 
 	h := makeHost(ctx)
-	destID, destAddr := makePeer(*dest)
-	_ = destID
-	_ = destAddr
+	destID, destAddr := utils.MakePeer(*dest)
 
 	h.Peerstore().AddAddr(destID, destAddr, 24*time.Hour)
-	kad, err := dht.New(ctx, h, dhtopts.Client(true), dhtopts.Validator(NullValidator{}))
-	_ = kad
+	kad, err := dht.New(ctx, h, dhtopts.Client(true), dhtopts.Validator(utils.NullValidator{}))
 	if err != nil {
 		log.Fatalf("Error creating DHT: %v\n", err)
 	}
